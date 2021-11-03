e8b4aff39718a1a33c6a8741eff9ba2e6d5be423
Francisco Nazar <francisco.nazartorres@gmail.com>
2021-11-03 15:28:11 -0300

    First commit
    Project backup

---
+++ Assets/Scripts/CongratScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{    
+    [SerializeField] private ParticleSystem sparksParticles;
+
+    private readonly List<string> textToDisplay = new List<string>();
+    private TextMesh text;
+    private float timeToNextText = 0f;
+    private int currentText = 0;
+    
+    void Start()
+    {
+        textToDisplay.Add("Congratulation");
+        textToDisplay.Add("All Errors Fixed");
+
+        text = GetComponent<TextMesh>();
+        text.text = textToDisplay[0];
+        
+        sparksParticles.Play();
+    }
+
+    void Update()
+    {
+        timeToNextText += Time.deltaTime;
+
+        if (timeToNextText > 1.5f)
+        {
+            timeToNextText = 0.0f;
+            
+            currentText++;
+            if (currentText >= textToDisplay.Count)
+            {
+                currentText = 0;
+            }
+
+            text.text = textToDisplay[currentText];
+        }
+    }
+}
